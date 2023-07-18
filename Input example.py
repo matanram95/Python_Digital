7ac26266d4594ce525f6a27d4e3db7e92d85b733
administrator <support@playsmart.co.il>
2023-07-18 16:07:53 +0300

    Input example

---
+++ Input example.py
@@ -0,0 +1,4 @@
+Name=input("Enter your name: ")
+Email=input("Enter your Email: ")
+Phone=input("Enter your Phone number: ")
+print("Wellcome " + Name + "\nYour Email account is " + Email + "\nWe send verification code to " + str(Phone))
